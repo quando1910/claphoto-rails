a52b778a9f0a2a8b4deb95d588f69f479a3b0b65
at-quando <quan.do@asiantech.vn>
2018-09-29 13:18:20 +0700

    fix

--- app/admin/prices.rb
+++ app/admin/prices.rb
@@ -1,11 +1,17 @@
 ActiveAdmin.register Price do
-  permit_params :num_pp, :price
+  permit_params :num_pp, :price, ,:offer ,:types ,:priceFirst ,:aspect ,:takenTime
 
   index do
     selectable_column
     id_column
     column :num_pp
     column :price
+    column :offer
+    column :types
+    column :priceFirst
+    column :aspect
+    column :takenTime
+
     actions
   end
 
@@ -13,6 +19,11 @@ ActiveAdmin.register Price do
     f.inputs do
       f.input :num_pp, label: "Number of people"
       f.input :price
+      f.input :offer
+      f.input :types
+      f.input :priceFirst
+      f.input :aspect
+      f.input :takenTime
     end
     f.actions
   end
